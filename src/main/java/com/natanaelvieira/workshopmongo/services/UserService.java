0d9b55b8b2774f271f75307fc0be0b3d3e1c83c3
NatanVieira <natanaelvieira130@gmail.com>
2019-04-30 21:05:18 -0300

    Obtendo usuario por Id

--- src/main/java/com/natanaelvieira/workshopmongo/services/UserService.java
+++ src/main/java/com/natanaelvieira/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.natanaelvieira.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.natanaelvieira.workshopmongo.domain.User;
 import com.natanaelvieira.workshopmongo.repository.UserRepository;
+import com.natanaelvieira.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
-	@Autowired
+	@Autowired 
 	private UserRepository repo;
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
 	}
 }
